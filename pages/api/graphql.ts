09411a5c0428c670ad9313bea58784bd03953a09
Asmita Gaire <asmitagaire00@gmail.com>
2025-03-12 22:12:54 +1100

    update

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,16 @@
 import { createSchema, createYoga } from "graphql-yoga";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { resolvers } from "../../graphql/resolvers";
 import { typeDefs } from "../../graphql/schema";
 
-export default createYoga<{ req: NextApiRequest; res: NextApiResponse }>({
-  schema: createSchema({ typeDefs, resolvers }),
+export default createYoga<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  schema: createSchema({
+    typeDefs,
+    resolvers,
+  }),
   graphqlEndpoint: "/api/graphql",
 });
 
