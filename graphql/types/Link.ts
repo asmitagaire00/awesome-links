6189603a8f101a61ec4a6bb128cce6b3f8baf871
Asmita Gaire <asmitagaire00@gmail.com>
2025-03-17 22:13:20 +1100

    defining queries using pothos

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -11,3 +11,15 @@ builder.prismaObject("Link", {
     users: t.relation("users"),
   }),
 });
+
+// 1.
+builder.queryField("links", (t) =>
+  // 2.
+  t.prismaField({
+    // 3.
+    type: ["Link"],
+    // 4.
+    resolve: (query, _parent, _args, _ctx, _info) =>
+      prisma.link.findMany({ ...query }),
+  })
+);
